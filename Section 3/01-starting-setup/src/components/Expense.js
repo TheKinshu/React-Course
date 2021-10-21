2bb538f9bfdf2b15b98313c5df8c620defd2baea
(Kelvin) Chun Kit Cho <kc007919@gmail.com>
2021-10-21 17:41:13 -0400

    Section 5 Complete

--- Section 3/01-starting-setup/src/components/Expense.js
+++ Section 3/01-starting-setup/src/components/Expense.js
@@ -1,32 +1,32 @@
+import React, { useState } from "react";
 import "./Expense.css";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./Expenses/ExpensesList";
+import ExpensesFilter from "./Filter/ExpensesFilter";
+
+import ExpensesChart from "./Expenses/ExpensesChart"
 
 function Expense(props) {
-    let expenses = props.expenses;
-    return (
-        <div className="expenses">
-            <ExpenseItem
-                title={expenses[0].title}
-                amount={expenses[0].amount}
-                date={expenses[0].date}
-            />
-            <ExpenseItem
-                title={expenses[1].title}
-                amount={expenses[1].amount}
-                date={expenses[1].date}
-            />
-            <ExpenseItem
-                title={expenses[2].title}
-                amount={expenses[2].amount}
-                date={expenses[2].date}
-            />
-            <ExpenseItem
-                title={expenses[3].title}
-                amount={expenses[3].amount}
-                date={expenses[3].date}
-            />
-        </div>
-    );
+  const [yearSelect, setYearSelected] = useState("2020");
+
+  const filterYear = (selectedYear) => {
+    setYearSelected(selectedYear);
+  };
+
+  const filterItems = props.items.filter(item => {
+    return item.date.getFullYear().toString() === yearSelect;
+  })
+
+  
+  return (
+    <div className="expenses">
+      <ExpensesFilter 
+        selected={yearSelect} 
+        onYearSelect={filterYear} 
+      />
+      <ExpensesChart expenses={filterItems}/>
+      <ExpensesList items={filterItems}/>
+    </div>
+  );
 }
 
 export default Expense;
