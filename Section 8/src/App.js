5332be14c143845beb896d4164dd91b10d454c9a
(Kelvin) Chun Kit Cho <kc007919@gmail.com>
2021-10-25 01:19:17 -0400

    Added UserList

--- Section 8/src/App.js
+++ Section 8/src/App.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import UserForm from "./components/Users/UserForm";
-
+import UserList from "./components/Users/UsersList";
 function App() {
-  return <UserForm />;
+  const [users, setUsers] = useState([]);
+
+  const addUserHandler = (enterUser) => {
+    setUsers((prevUsers) => {
+      const userList = [...prevUsers];
+      userList.unshift({
+        id: Math.floor(Math.random() * 1000).toString(),
+        ...enterUser,
+      });
+      return userList;
+    });
+  };
+
+  return (
+    <div>
+      <UserForm addUsers={addUserHandler} />
+      <UserList users={users} />
+    </div>
+  );
 }
 
 export default App;
--- Section 8/src/components/Users/UserForm.js
+++ Section 8/src/components/Users/UserForm.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import styles from "./UserForm.module.css";
 
 const UserForm = (props) => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+
+  const usernameHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const ageHandler = (event) => {
+    setAge(event.target.value);
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      return;
+    }
+    if (+age < 1) {
+      return;
+    }
+    const userInfo = {
+      username: username,
+      age: age,
+    };
+    props.addUsers(userInfo);
+    setUsername("");
+    setAge("");
   };
 
   return (
@@ -13,9 +37,14 @@ const UserForm = (props) => {
       <form onSubmit={addUserHandler}>
         <div>
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" />
+          <input
+            id="username"
+            value={username}
+            type="text"
+            onChange={usernameHandler}
+          />
           <label htmmlFor="age">Age (Years)</label>
-          <input id="age" type="number" />
+          <input id="age" type="number" value={age} onChange={ageHandler} />
           <Button type="submit">Add User</Button>
         </div>
       </form>
